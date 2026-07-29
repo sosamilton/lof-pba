1a8822ed41b76761c45c37cd28933c29dc761b9a
Milton Sosa <milton.sosa.22@gmail.com>
2026-07-29 15:54:03 -0300

    feat: reusable UI components and automated tests
    - Add MessageBanner.svelte (error/notice banner)
    - Add EmptyState.svelte (empty state with optional action)
    - Add Vitest with 39 tests: utils.test.js, personas.test.js, grist.test.js
    - Add test script to package.json

---
+++ src/lib/components/MessageBanner.svelte
@@ -0,0 +1,10 @@
+<script>
+  let { error = '', notice = '' } = $props()
+</script>
+
+{#if error}
+  <div class="msg error">{error}</div>
+{/if}
+{#if notice}
+  <div class="msg notice">{notice}</div>
+{/if}
